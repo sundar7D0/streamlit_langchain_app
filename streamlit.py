8c8a36ae804c403f8ab1bbdeb221f24b8c36908e
Sundar Raman P <43384356+sundar7D0@users.noreply.github.com>
2023-03-23 13:53:14 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -95,45 +95,97 @@ def escape_ansi(line):
     return re.compile(r'(\x9B|\x1B\[)[0-?]*[ -\/]*[@-~]').sub('', line)
 
 
-template = """If someone asks you to perform a task, your job is to come up with a series of Selenium Python commands that will perform the task. 
-There is no need to need to include the descriptive text about the program in your answer, only the commands.
-Note that the version of selenium is 4.7.2.
-find_element_by_class_name is deprecated.
-Please use find_element(by=By.CLASS_NAME, value=name) instead.
-You must use detach option when webdriver
-You must starting webdriver with --lang=en-US
+#template = """If someone asks you to perform a task, your job is to come up with a series of Selenium Python commands that will perform the task. 
+#There is no need to need to include the descriptive text about the program in your answer, only the commands.
+#Note that the version of selenium is 4.7.2.
+#find_element_by_class_name is deprecated.
+#Please use find_element(by=By.CLASS_NAME, value=name) instead.
+#You must use detach option when webdriver
+#You must starting webdriver with --lang=en-US
+#Begin!
+#Your job: {question}
+#"""
+
+system_template="""Use the following pieces of context to answer the users question. 
+{context}
+If you don't know the answer, just say that you don't know, don't try to make up an answer.
+EACH AND EVERY output sentence that you generate should be in the following format:
+'''
+<
+  sentence1: <Generated sentence2>
+  SOURCE_IDS: <List of SOURCE_ID used to generate the above sentence1>
+>
+<
+  sentence2: <Generated sentence2>
+  SOURCE_IDS: <List of SOURCE_ID used to generate the above sentence2>
+>
+...(can continue n times)
+'''
+ALWAYS return a "SOURCE_IDS" as a part of each and every output sentence so that user benefits from your reply. The "SOURCE_ID" part should be a reference to the source of the document from which you got your answer.
 
 Begin!
-Your job: {question}
+----------------
 """
+messages = [
+    SystemMessagePromptTemplate.from_template(system_template),
+    HumanMessagePromptTemplate.from_template("{question}")
+]
+prompt = ChatPromptTemplate.from_messages(messages)
+
+def chat_only(chat_bot,input):
+    return chat_bot([HumanMessage(content=input)])
+
+def retriever(input):
+    index_name="kira-demo4"
+    embeddings = OpenAIEmbeddings()
+    docsearch = Pinecone.from_existing_index(index_name, embeddings)
+    results = docsearch.similarity_search_with_score(input,k=10)
+    output  = "\n".join("\nOutput {}: \n 1. Relevancy Score - {} \n 2. Source - {} \n 3. Content - \n {}\n\n\n".format(i,result[1],result[0].metadata,result[0].page_content) for i, result in enumerate(results))
+    return output
+
+def retriever_chat(chat_bot,input,prompt):
+    results = retriever(input)
+    context="\n".join('\nSOURCE_ID: {} \n'.format(i)+("{"+result[0].page_content+"}").replace('\n',',\n') for i, result in enumerate(results))
+    llm_chain = LLMChain(llm=chat_bot, prompt=prompt)
+    return llm_chain.run({"context": context, "question": input})  #, return_only_outputs=True)
+
+def excel_analyst(chat_bot,input):
+    df_agent = create_pandas_dataframe_agent(chat_bot, df, verbose=True)  #,model_name="gpt-3.5-turbo"
+    return df_agent.run(input)
 
 st.set_page_config(layout="wide") 
-st.title('🐧 Demo: Knowledge-grounded Intelligent Research Assistant (KIRA) to help with search and analytics over private documents and public web data!')
+st.title('🐧 KIRA: Knowledge-grounded Intelligent RA which helps with search and analytics over private documents & public web data!')
 col1, col2 = st.columns(2)
 
 with col1:
-    openai_api_key = st.text_input(label="OpenAI API key", placeholder="Input your OpenAI API key here:",type="password")
+    #openai_api_key = st.text_input(label="OpenAI API key", placeholder="Input your OpenAI API key here:",type="password")
     question = st.text_area(
         label = "Input" ,
         placeholder = "e.g. Go to https://www.google.com/ and search for GPT-3"
     )
-    agent_list = ["Retriever","Writer","Analyst"]
+    agent_list = ["Chat","Retriever","Writer","Excel_Analyst"]
     agent = st.radio("Agent",agent_list)
     start_button = st.button('Run')
             
 with col2:
     if start_button:
         with st.spinner("Running..."):
-            llm=OpenAI(temperature=0,openai_api_key=openai_api_key)
-            chain = PALChain.from_colored_object_prompt(llm, verbose=True)
+            chat = PromptLayerChatOpenAI(pl_tags=["langchain"])
+            #llm=OpenAI(temperature=0,openai_api_key=openai_api_key)
+            #chain = PALChain.from_colored_object_prompt(llm, verbose=True)
             output = st.empty()
-            if agent == "Retriever":
+            if agent == "chat":
                 with st_capture(output.code):
-                    index_name="kira-demo4"
-                    embeddings = OpenAIEmbeddings()
-                    docsearch = Pinecone.from_existing_index(index_name, embeddings)
-                    results = docsearch.similarity_search_with_score(question,k=10)
-                    print("\n".join("\nOutput {}: \n 1. Relevancy Score - {} \n 2. Source - {} \n 3. Content - {}\n".format(i,result[1],result[0].metadata,result[0].page_content) for i, result in enumerate(results)))
+                    print(chat_only(chat,question))
+            elif agent == "Retriever":
+                with st_capture(output.code):
+                    print(retriever(question))
+            elif agent == "Writer":
+                with st_capture(output.code):
+                    print(retriever_chat(chat,question,prompt))
+            elif agent == "Excel_Analyst":
+                with st_capture(output.code):
+                    print(excel_analyst(chat,question))
 #            with st_capture(output.code):
 #                prompt = PromptTemplate(
 #                    template=template,
